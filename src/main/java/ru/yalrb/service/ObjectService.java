eb9b34bebb46fec402e8d9d2fa749608a0420ac0
MikhailTulupov <kd.maikl-201@yandex.ru>
2024-03-05 15:03:12 +0900

    Refactoring service

--- src/main/java/ru/yalrb/service/ObjectService.java
+++ src/main/java/ru/yalrb/service/ObjectService.java
@@ -1,10 +1,9 @@
 package ru.yalrb.service;
 
-import ru.yalrb.dto.ObjectDTO;
 import ru.yalrb.model.Object;
 
 /**
  * This interface provides a methods for manipulating {@link Object} data.
  */
-public interface ObjectService extends CrudService<ObjectDTO> {
+public interface ObjectService extends CrudService<Object> {
 }
